7f8908dbc307ead460ad48a7acf1eac8234bb266
MohamedAshrafzZ <mashrafzz957@gmail.com>
2026-04-10 18:14:50 +0200

    Question 1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
-﻿namespace G_NET_12_LINQ01
+﻿using G_NET_12_LINQ01.Models;
+
+namespace G_NET_12_LINQ01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 1 
+            //            1.Get all products from the "Seafood" category.Print each
+            //product's name and price.
+
+            //Answer:
+            var result = Source.ProductList.Where(p => p.Category == "Seafood")
+                        .Select(p => new { p.ProductName, p.UnitPrice });
+
+
+            #endregion
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
